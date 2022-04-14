24f512d91d79d16b4d26eebb0be66083d17929b4
TheOmnimax <max@maxshaberman.com>
2022-04-13 23:07:55 -0400

    Added loading indicator to download page

--- lib/screens/download_ebooks_screen/bloc/download_ebooks_state.dart
+++ lib/screens/download_ebooks_screen/bloc/download_ebooks_state.dart
@@ -1,8 +1,25 @@
 import 'package:equatable/equatable.dart';
 import 'package:ereader/utils/file_explorer/ebook_metadata.dart';
+import 'package:ereader/constants/constants.dart';
 
 abstract class DownloadEbooksState extends Equatable {
-  const DownloadEbooksState();
+  const DownloadEbooksState({
+    this.ebookList = const <EbookMetadata>[],
+    this.status = LoadingStatus.working,
+    this.info = 'No status',
+  });
+  final List<EbookMetadata> ebookList;
+  final LoadingStatus status;
+  final String info;
+
+  @override
+  List<Object?> get props => [ebookList, status, info];
+
+  DownloadEbooksState copyWith({
+    List<EbookMetadata>? ebookList,
+    LoadingStatus? status,
+    String? info,
+  });
 }
 
 class InitialState extends DownloadEbooksState {
@@ -10,22 +27,47 @@ class InitialState extends DownloadEbooksState {
 
   @override
   List<Object?> get props => [];
-}
-
-class Ready extends DownloadEbooksState {
-  const Ready();
 
   @override
-  List<Object?> get props => [];
+  InitialState copyWith({
+    List<EbookMetadata>? ebookList,
+    LoadingStatus? status,
+    String? info,
+  }) {
+    return const InitialState();
+  }
 }
 
 class ListBooks extends DownloadEbooksState {
   const ListBooks({
-    required this.ebookList,
-  });
-
-  final List<EbookMetadata> ebookList;
+    required List<EbookMetadata> ebookList,
+    LoadingStatus status = LoadingStatus.ready,
+    String info = 'Ready!',
+  }) : super(
+          ebookList: ebookList,
+          status: status,
+          info: info,
+        );
 
   @override
-  List<Object?> get props => [ebookList];
+  List<Object?> get props => [ebookList, status];
+
+  @override
+  ListBooks copyWith({
+    List<EbookMetadata>? ebookList,
+    LoadingStatus? status,
+    String? info,
+  }) {
+    return ListBooks(
+      ebookList: ebookList ?? this.ebookList,
+      status: status ?? this.status,
+      info: info ?? this.info,
+    );
+  }
 }
+
+// class Downloading extends DownloadEbooksState {
+//   const Downloading({
+//     required List<EbookMetadata> ebookList,
+//   }) : super(ebookList: ebookList);
+// }
